8b50fca5400064ceccf4c48a535c8dd3df1dbee3
Yasiru Kavinda <yasiru0210@gmail.com>
2024-08-25 09:34:22 +0530

    second

--- src/main/java/edu/icet2/service/StudentServiceImpl2.java
+++ src/main/java/edu/icet2/service/StudentServiceImpl2.java
@@ -3,19 +3,20 @@ package edu.icet2.service;
 import edu.icet2.dto.Student;
 import edu.icet2.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Primary
 @Service
 
-public class StudentServiceImpl implements StudentService {
+public class StudentServiceImpl2 implements StudentService {
     @Autowired
     StudentRepository repository;
 
     public List<Student> getStudent() {
-       return repository.findAll();
 
+        return repository.findAll();
     }
 
     @Override
